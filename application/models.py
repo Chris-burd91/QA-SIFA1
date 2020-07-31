9e7187f8454269c04cb6cfe8d81c84306e72d89d
chris-burd91 <chrisburd91@gmail.com>
2020-07-31 13:02:41 +0000

    working on models.py

--- application/models.py
+++ application/models.py
@@ -9,3 +9,9 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
+
+class Orders(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    ShippedDate = db.Column
