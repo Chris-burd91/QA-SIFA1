0c19d068bde70012d62c5e5cad571928c930385a
chris-burd91 <chrisburd91@gmail.com>
2020-08-06 13:56:26 +0000

    finally added login functionality

--- application/models.py
+++ application/models.py
@@ -5,8 +5,62 @@ from datetime import datetime
 
 
 class User(db.Model, UserMixin):
-    user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    orders = db.relationship('Orders', backref='user', lazy=True)
+   
+    def __repr__(self):
+        return ''.join([
+            'User ID: ', str(self.id), '\r\n',
+            'Username: ', self.username, '\r\n',
+            'Name: ', self.first_name, ' ', self.last_name
+        ])
 
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+order_stock = db.Table('order_stock',
+     db.Column('order_id', db.Integer, db.ForeignKey('orders.order_id')),
+     db.Column('stock_id',db.Integer, db.ForeignKey('stock.stock_id')),
+     db.PrimaryKeyConstraint('order_id', 'stock_id')
+)
+
+     
+class Orders(db.Model):
+    order_id = db.Column(db.Integer, primary_key=True)
+    date_order = db.Column(db.Integer, nullable=False, default=datetime.utcnow)
+    order_status = db.Column(db.String(40), default='Pending')
+    customer_name = db.Column(db.String(50), nullable=False)
+    customer_address = db.Column(db.String(140),nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+ 
+    def __repr__(self):
+       return ''.join([
+           'Order ID: ', str(self.id), '\r\n',
+           'Customer Details: ', self.customer_name, '\r\n', self.customer_address, '\r\n',
+           'Order Status: ', self.order_status, '\r\n'
+       ])
+
+class Stock(db.Model):
+    stock_id = db.Column(db.Integer, primary_key=True)
+    product_name = db.Column(db.String(50), nullable=False)
+    product_discription = db.Column(db.String(500))
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float(10), nullable=False)
+    sell_price = db.Column(db.Float(10), nullable=False)
+  
+    def __repr__(self):
+        return ''.join([
+            'Stock ID: ', str(self.id), '\r\n',
+            'Product Info: ', self.product_name, '\r\n', self.product_discription, '\r\n',
+            'Quantity: ',str(self.quantity), '\r\n',
+            'Prices: ', 'Bought: ',str(self.price), '\r\n', 'Sold: ',str(self.sell_price), '\r\n',
+            'Serial_No.: ',str(self.serial_no)
+        ])       
--- application/routes.py
+++ application/routes.py
@@ -1,41 +1,105 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request, flash
 
-from application import app
+from application import app, db, bcrypt
+from application.models import User
+from application.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html', title ='Home')
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='Register')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash_pw = bcrypt.generate_password_hash(form.password.data)
 
-@app.route('/account')
+        user = User(
+           first_name=form.first_name.data,
+           last_name=form.last_name.data,
+           email=form.email.data,
+           password=hash_pw
+           
+       )
+        db.session.add(user)
+        db.session.commit()
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name
+        form.email.data = current_user.email
+    return render_template('account.html', title ='Account', form=form)
 
-@app.route('/login')
+
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html', title='Login')
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
 
 @app.route('/orders')
+@login_required
 def orders():
     return render_template('orders.html', title='Orders')
 
 @app.route('/add_edit_orders')
+@login_required
 def add_edit_orders():
     return render_template('add_edit_orders.html', title='Add/Edit Orders')
 
 @app.route('/stock')
+@login_required
 def stock():
     return render_template('stock.html', title='Stock')
 
 @app.route('/add_edit_stock')
+@login_required
 def add_edit_stock():
     return render_template('add_edit_stock.html', title='Add/Edit Stock')
 
-@app.route("/logout")
-def logout():
-
-    return redirect(url_for('login'))
+@app.route("/account/delete", methods=["GET", "POST"])
+@login_required
+def account_delete():
+    user = current_user.id
+    account = User.query.filter_by(id=user).first()
+    logout_user()
+      
+    db.session.delete(account)
+    db.session.commit()
+    return redirect(url_for('register'))
