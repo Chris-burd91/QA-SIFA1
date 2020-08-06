500486202a1846e57e6d529cd3d09ed7e5b0b446
chris-burd91 <chrisburd91@gmail.com>
2020-08-06 09:44:53 +0000

    starting from base again

--- application/models.py
+++ application/models.py
@@ -9,3 +9,4 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+
--- application/routes.py
+++ application/routes.py
@@ -5,4 +5,37 @@ from application import app
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title ='Home Page')
+    return render_template('home.html', title ='Home')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+
+@app.route('/account')
+def account():
+    return render_template('account.html', title='Account')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route('/orders')
+def orders():
+    return render_template('orders.html', title='Orders')
+
+@app.route('/add_edit_orders')
+def add_edit_orders():
+    return render_template('add_edit_orders.html', title='Add/Edit Orders')
+
+@app.route('/stock')
+def stock():
+    return render_template('stock.html', title='Stock')
+
+@app.route('/add_edit_stock')
+def add_edit_stock():
+    return render_template('add_edit_stock.html', title='Add/Edit Stock')
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
